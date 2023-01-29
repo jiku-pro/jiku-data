686dc98a06738da54eeca6ee4ebdc89af4c4a903
Jiku-Pro <0todd0@gmail.com>
2023-01-30 07:14:58 +0900

    Add an anova1 dataset

---
+++ src/jikudata/datasets/Southampton1/__init__.py
@@ -0,0 +1,32 @@
+
+import os
+import numpy as np
+from ... _cls import _Dataset, ExpectedResults, SPM1DParameters
+
+
+
+class Southampton1(_Dataset):
+	
+	def _set_attrs(self):
+		self.datafile   = os.path.join(  os.path.dirname( __file__ ), 'data.csv'  )
+		self.www        = 'https://www.southampton.ac.uk/~cpd/anovas/datasets/Doncaster&Davey%20-%20Model%201_1%20One%20factor.txt'
+		
+	def _set_expected(self):
+		e             = ExpectedResults()
+		e.STAT        = 'F'
+		e.z           = 17.08
+		e.df          = (2, 21)
+		e.p           = 0.0001
+		e.tol.z       = 0.01
+		e.tol.df      = 1e-05
+		e.tol.p       = 0.001
+		self.expected = e
+		
+	def _set_params(self):
+		self.params                  = SPM1DParameters()
+		self.params.testname         = 'anova1'
+		self.params.args             = self.y, self.x
+		self.params.inference_args   = (0.05,)
+		self.params.inference_kwargs = dict(method='param')
+
+
