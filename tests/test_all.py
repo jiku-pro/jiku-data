9b8ed511b39265fbbea11ae3ed8536e8a2b9b221
Jiku-Pro <todd.pataky.jiku.pro@gmail.com>
2023-09-07 06:07:59 +0900

    Update unit test script

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,9 +6,15 @@ import jikudata as jd
 # 	dataset.runtest( verbose=True )
 
 
-for dataset in jd.datasets.iter_all():
-	if dataset.dim == 0:
-		if dataset.name == 'SPM1D_ANOVA3ONERM_2x3x4':
-			continue
-		dataset.runtest( verbose=True )
 
+# for dataset in jd.datasets.iter_all():
+# 	if dataset.dim == 0:
+# 		if dataset.name == 'SPM1D_ANOVA3ONERM_2x3x4':
+# 			continue
+# 		dataset.runtest( verbose=True )
+
+
+def test_all():
+	for dataset in jd.datasets.iter_all():
+		if dataset.params.testname in ['ttest', 'ttest_paired', 'ttest2', 'anova1']:
+			dataset.runtest( verbose=True )
